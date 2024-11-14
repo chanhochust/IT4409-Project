7e68c238dfa74747feef9b3059a74abed7db07c2
hariki <nxh0809@gmail.com>
2024-11-14 16:14:14 +0700

    Update to ESLint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,80 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...compat.extends(
+    'next/core-web-vitals',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:tailwindcss/recommended',
+  ),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+    },
+
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: 'script',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-unsafe-enum-comparison': 'error',
+      'react/prefer-read-only-props': 'warn',
+      '@typescript-eslint/await-thenable': 'error',
+      '@typescript-eslint/no-deprecated': 'warn',
+      '@typescript-eslint/no-unsafe-declaration-merging': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      'tailwindcss/classnames-order': 'off',
+      'react-hooks/exhaustive-deps': 'error',
+      'prefer-template': 'error',
+      'no-console': [
+        'warn',
+        {
+          allow: ['error', 'warn', 'info'],
+        },
+      ],
+      'react/jsx-curly-brace-presence': [
+        'error',
+        {
+          props: 'never',
+          children: 'ignore',
+        },
+      ],
+      eqeqeq: 'error',
+      'react/jsx-sort-props': [
+        'error',
+        {
+          callbacksLast: true,
+          shorthandFirst: true,
+          noSortAlphabetically: false,
+          reservedFirst: true,
+        },
+      ],
+      'no-restricted-imports': [
+        'warn',
+        {
+          patterns: ['../../*'],
+        },
+      ],
+    },
+  },
+];
