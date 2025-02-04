6c58c0183742d00717c3b70016cedb0ef07dc03e
Hariki <nxh0809@gmail.com>
2025-02-05 01:57:48 +0700

    Fix lint issues

--- src/shared/components/progress/AppProgress.tsx
+++ src/shared/components/progress/AppProgress.tsx
@@ -9,7 +9,7 @@ const Progress = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
 >(({ className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
-    className={cn('relative h-4 w-full overflow-hidden rounded-full bg-secondary', className)}
+    className={cn('bg-secondary relative h-4 w-full overflow-hidden rounded-full', className)}
     ref={ref}
     {...props}>
     <ProgressPrimitive.Indicator
